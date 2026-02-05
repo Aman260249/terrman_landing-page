70636ffa0bab12c84f67f657d1197e64a02039ac
Aman260249 <aman26024@gmail.com>
2026-02-05 14:19:46 +0530

    fix: final prisma setup for vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,27 @@
-// import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
-const connectionString = process.env.DATABASE_URL; // Ensure this is the 6543 port
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
 const pool = new pg.Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ 
-  adapter,
-  log: ['query', 'error'] // Adding 'error' helps see the real SQL issue in terminal
-});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ['error'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
